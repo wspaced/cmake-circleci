11501ecfd60362b1103932155196a038f5d7d171
wspaced <white.space.daemon@icloud.com>
2017-10-15 15:08:03 +0900

    Add google test and circle ci config.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+TEST(MyTest, AlwaysFalseTest) {
+    ASSERT_FALSE(true);
 }
